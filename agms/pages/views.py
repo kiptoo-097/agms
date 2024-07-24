e2eb1bba02932a463ba850bb8b087fccba7f1f45
kiptoo-097 <kiptoo097@gmail.com>
2024-07-24 16:09:25 +0300

    Created pages view for about, staff, contact

--- agms/pages/views.py
+++ agms/pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def staff(request):
+    return render(request, 'pages/staff.html')
+
+def contact(request):
+    return render(request, 'pages/contact.html')
+
