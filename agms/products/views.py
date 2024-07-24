27ec4271ef1dd76bccea5e93be5dce933b95aeab
kiptoo-097 <kiptoo097@gmail.com>
2024-07-24 15:54:59 +0300

    Created views for product categories, product list by category, and individual product detail

--- agms/products/views.py
+++ agms/products/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
 
-# Create your views here.
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'products/category_list.html', {'categories': categories})
+
+def product_list(request, category_slug=None):
+    category = None
+    products = Product.objects.all()
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category)
+    return render(request, 'products/product_list.html', {'category': category, 'products': products})
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'products/product_detail.html', {'product': product})
